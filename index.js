b2c5a086d9e0de2e33c070cf36fc7d584ca6f599
John Ritsema <john_ritsema@yahoo.com>
2015-08-04 23:21:31 -0400

    added title to BrowserWindow

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 var app = require('app');  // Module to control application life.
 var BrowserWindow = require('browser-window');  // Module to create native browser window.
 
-// Report crashes to our server.
-//require('crash-reporter').start();
-
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is GCed.
 var mainWindow = null;
@@ -20,8 +17,14 @@ app.on('window-all-closed', function() {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 app.on('ready', function() {
+  
   // Create the browser window.
-  mainWindow = new BrowserWindow({ width: 1280, height: 750 });
+  mainWindow = new BrowserWindow({ 
+    title: 'notemark',
+    width: 1280, 
+    height: 750, 
+    frame: true 
+  });
 
   // and load the index.html of the app.
   mainWindow.loadUrl('file://' + __dirname + '/index.html');
